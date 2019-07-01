04aeb605267f9c4783e0884312ed7319e39367ca
luisandia <luisandiamamani@gmail.com>
2019-07-01 17:49:26 -0500

    very very chat finished

--- basics/chat.js
+++ basics/chat.js
@@ -3,10 +3,7 @@ const app = express()
 const socketio = require('socket.io');
 
 
-
-
 app.use(express.static(__dirname + '/public'));
-
 const expressServer = app.listen(9000);
 const io = socketio(expressServer, {
     path: '/socket.io',
@@ -14,8 +11,11 @@ const io = socketio(expressServer, {
 })
 io.on('connection', (socket) => {
     socket.emit('messageFromServer', { data: 'Welcome to the socket io server' });
-
     socket.on('messageToServer', (dataFromClient) => {
         console.log(dataFromClient);
+    })
+    socket.on('newMessageToServer', (msg) => {
+        console.log(msg);
+        io.emit('messageToClients', { ...msg })
     })
 })
